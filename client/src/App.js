71b12dae8d2699bfeafc80808db6edad8e90c94d
kongyb <kongyb08@naver.com>
2021-12-28 22:07:41 +0900

    basic server client

---
+++ client/src/App.js
@@ -0,0 +1,81 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import {useEffect} from 'react';
+
+function App() {
+
+  const getTest=()=>{
+    axios({
+      method:'GET',
+      url:process.env.REACT_APP_SERVER_URL,
+    }).then((res)=>{
+      console.log(res.data);
+    })
+  }
+
+  const postTest=()=>{
+    axios({
+      method:'POST',
+      url:process.env.REACT_APP_SERVER_URL,
+    }).then((res)=>{
+      console.log(res.data);
+    })
+  }
+
+  const putTest=()=>{
+    axios({
+      method:'PUT',
+      url:process.env.REACT_APP_SERVER_URL,
+    }).then((res)=>{
+      console.log(res.data);
+    })
+  }
+
+  const patchTest=()=>{
+    axios({
+      method:'PATCH',
+      url:process.env.REACT_APP_SERVER_URL,
+    }).then((res)=>{
+      console.log(res.data);
+    })
+  }
+
+  const deleteTest=()=>{
+    axios({
+      method:'DELETE',
+      url:process.env.REACT_APP_SERVER_URL,
+    }).then((res)=>{
+      console.log(res.data);
+    })
+  }
+
+  useEffect(()=>{
+    getTest();
+    postTest();
+    putTest();
+    patchTest();
+    deleteTest();
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express=require('express');
+const cors=require('cors');
+
+const app=express();
+const PORT=80;
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods:'*'
+  })
+)
+
+app.get('/',function(req,res){
+  res.send('response for GET request');
+})
+
+app.post('/',function(req,res){
+  res.send('response for POST request');
+})
+
+app.patch('/',function(req,res){
+  res.send('response for PATCH request');
+})
+
+app.put('/',function(req,res){
+  res.send('response for PUT request');
+})
+
+app.delete('/',function(req,res){
+  res.send('response for DELETE request');
+})
+
+app.listen(PORT,()=>{
+  console.log('server running');
+})
