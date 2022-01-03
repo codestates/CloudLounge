2d1684edf87d0a2f0b55a373d454d48dacd57ef4
Jun0922 <shaq94@khu.ac.kr>
2022-01-03 13:08:15 +0900

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,6 @@ import './App.css'
 import axios from 'axios'
 import { useEffect } from 'react'
 import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { handleLogin, changeLocation } from './actions'
-//useSelector, useDispatch 모두 훅이기 때문에 컴포넌트 안에서 실행되어야함
-//redux 스테이트 확인하는 법 => useSelector
-// const isLoginState = useSelector((state) => state.isLoginReducer)
-//   const locationState = useSelector((state) => state.locationReducer)
-//   console.log('islogin==>>', isLoginState)
-//   console.log('location===', locationState)
-//redux dispatch로 원하는 action 사용하는법
-//   const dispatch = useDispatch()
-//   dispatch(handleLogin())
-//   dispatch(changeLocation())
-//   dispatch(handleLogin())
-//   dispatch(changeLocation())
 function App() {
   return (
     <div className="App">
