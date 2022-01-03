a16456004e49e607b90f82271a101b099279b1fe
kongyb <kongyb08@naver.com>
2022-01-03 10:17:36 +0900

    kakao map apply

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navBar">
+      <div className="navBtn">홈</div>
+      <div className="navBtn">MyPage</div>
+      <div className="navBtn">로그인</div>
+    </div>
+  )
+}
+
+export default Navbar
