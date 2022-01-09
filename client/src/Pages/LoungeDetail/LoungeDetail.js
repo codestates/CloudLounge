3f26b01d6bc26ed049b9c195f022b71674fcf9d8
kongyb <kongyb08@naver.com>
2022-01-09 18:16:44 +0900

    write comment btn ; ;

--- client/src/Pages/LoungeDetail/LoungeDetail.js
+++ client/src/Pages/LoungeDetail/LoungeDetail.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Fragment, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './LoungeDetail.css'
 import Comment from '../../Components/Comment'
 import loungeInfo from '../../dummy/sampledata'
@@ -9,44 +9,49 @@ import commentdummy from './commentdummy'
 
 const LoungeDetail = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   let loungeId = localStorage.getItem('loungeId')
   //lounge정보를 가져오는 코드 axios이용
   let loungeDetail = loungeInfo[loungeId - 1]
   dispatch(setLounge(loungeDetail))
-  console.log(loungeDetail)
+  const commentBtnClick = () => {
+    navigate('/comment')
+  }
   return (
-    <div className="details">
-      <img className="detailPic" src={loungeDetail.image} />
-      <div className="detailAddress">{loungeDetail.address}</div>
-      <div className="detailRatingText">별점: {loungeDetail.avgStars}점</div>
-      <div className="detailRating">
-        <div className="star-fill" style={{ width: `${loungeDetail.avgStars * 20}%` }}>
-          <span>★</span>
-          <span>★</span>
-          <span>★</span>
-          <span>★</span>
-          <span>★</span>
+    <Fragment>
+      <div className="details">
+        <img className="detailPic" src={loungeDetail.image} />
+        <div className="detailAddress">{loungeDetail.address}</div>
+        <div className="detailRatingText">별점: {loungeDetail.avgStars}점</div>
+        <div className="detailRating">
+          <div className="star-fill" style={{ width: `${loungeDetail.avgStars * 20}%` }}>
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+          </div>
+          <div className="star-base">
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+            <span>★</span>
+          </div>
         </div>
-        <div className="star-base">
-          <span>★</span>
-          <span>★</span>
-          <span>★</span>
-          <span>★</span>
-          <span>★</span>
-        </div>
-      </div>
-      <div className="comments">
-        {/* {loungeDetail.comments.map((el, index) => (
+        <div className="comments">
+          {/* {loungeDetail.comments.map((el, index) => (
           <Comment key={index} comment={el} />
         ))} */}
-        {commentdummy.map((el, index) => (
-          <Comment key={index} comment={el} />
-        ))}
+          {commentdummy.map((el, index) => (
+            <Comment key={index} comment={el} />
+          ))}
+        </div>
       </div>
-      <Link to="/comment">
-        <img src="./iconComment.png" className="writeComment" />
-      </Link>
-    </div>
+      <div className="btnContainer">
+        <img src="./iconComment.png" className="writeComment" onClick={commentBtnClick} />
+      </div>
+    </Fragment>
   )
 }
 
